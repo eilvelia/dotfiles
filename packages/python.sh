be322cfdfcb5c63e7443e147903bb1cd8df29ebb
Bannerets <comonoid@protonmail.com>
2020-09-02 14:00:15 +0000

    Update packages/python.sh

--- packages/python.sh
+++ packages/python.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-pip2 install --user neovim
+if command -v pip2 &> /dev/null; then
+  pip2 install --user neovim
+else
+  echo "Warning: pip2 is not installed."
+fi
 
 pip3 install --user neovim
-pip3 install neovim-remote
+pip3 install --user neovim-remote
