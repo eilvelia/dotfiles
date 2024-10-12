af5ab9757a66d33559c69e470feb7c7b0a0d6a7d
eilvelia <hi@eilvelia.cat>
2024-10-12 13:47:20 +0000

    0x0-paste.fish: add file uploading

--- fish/functions/0x0-paste.fish
+++ fish/functions/0x0-paste.fish
@@ -1,8 +1,17 @@
 function "0x0-paste" --description "Paste to 0x0.st"
+  # Syntax:
+  # 0x0-paste [-f <filename>] [<file>]
+  # <file> is the file to send to 0x0.st; by default reads from stdin.
+  # <filename> is the name of the file that is sent to 0x0.st; defaults either
+  #            to <file> (if set) or paste.txt.
+  argparse 'f=' -- $argv
   if set -q argv[1]
+    set -f file $argv[1]
     set -f filename $argv[1]
   else
+    set -f file "-"
     set -f filename "paste.txt"
   end
-  curl -F "file=@-;filename=$filename" '-Fsecret=' https://0x0.st
+  set -q _flag_f; and set -f filename $_flag_f
+  curl -F "file=@$file;filename=$filename" '-Fsecret=' https://0x0.st
 end
